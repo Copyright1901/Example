d1e9b1c120e2d6c66e2f4de8287d521e65a88eb7
Copyright1901 <102325143+Copyright1901@users.noreply.github.com>
2022-03-25 20:09:00 -0300

    Update OptionSelectorLevelMarker.c

--- Example/scripts/5_Mission/gui/newui/OptionSelectorLevelMarker.c
+++ Example/scripts/5_Mission/gui/newui/OptionSelectorLevelMarker.c
@@ -1,3 +1,21 @@
+/**
+* OptionSelectorLevelMarker.c 
+* 
+* TH
+* www.thebritto.com.br 
+* © 2019 - 2021 TH 
+* 
+* Last Modified: 24/11/2021 
+* Modified By: TH
+* Created: 07/09/2019
+* 
+* This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License. 
+* To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/. 
+* This work is property of TH. You do not have permissions to edit/distribute any of this content without written permission from the content author (TH). 
+* Failure to adhere to the content license(s) above will result in actions from Bohemia Interactive Studios and/or Valve.
+* 
+*/
+
 modded class OptionSelectorLevelMarker extends OptionSelectorSliderSetup
 {
 	void OptionSelectorLevelMarker(Widget parent, float value, ScriptedWidgetEventHandler parentMenu, bool disabled, float min, float max)
